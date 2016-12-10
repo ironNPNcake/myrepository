233dad136a5b2928b06506baf63c65390691971f
ironNPNcake <jakub.sadlo@outlook.com>
2016-12-10 04:05:33 +0100

    optymaliz

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using System.Windows;
+//using System.Windows;
 
 namespace ConsoleApplication
 {
@@ -11,7 +11,6 @@ namespace ConsoleApplication
             klasa.pisz();
             
             Console.ReadKey();
-            char a;
             
         }
     }
